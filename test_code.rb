2efea156f6ea74c6b4f92f8f1bdcc3b953ac46ef
dylan <dylan.sarikas@gmail.com>
2022-01-12 17:53:36 -0600

    fixes the output table

--- test_code.rb
+++ test_code.rb
@@ -13,12 +13,31 @@ table = TTY::Table.new(["Name","Price", "Description", "URL"], [["Green Bay Pack
 
 #table = TTY::Table.new(["Name","Price", "Description", "URL"]. [[],[]])
 
-puts table.render(:ascii)
+#puts table.render(:ascii)
 
 product_names = []
+price_amounts = []
+description_tags = []
+url_locations = []
 products.each do |product|
   product_names << product["name"]
+  price_amounts << product["price"]
+  description_tags << product["description"]
+  url_locations << product["image_url"]
 end
 
-p product_names
+# p price_amounts
+# p description_tags
+# p url_locations
+
+table2 = TTY::Table.new
+table2 << product_names
+table2 << price_amounts
+table2 << description_tags
+#table2 << url_locations
+
+puts table2.render(:ascii)
+
+#p product_names
+
 #table << product_names
