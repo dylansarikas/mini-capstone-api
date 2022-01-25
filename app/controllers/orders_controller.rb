33734f7d64699f72284297761d8ef9fb23eefa54
dylan <dylan.sarikas@gmail.com>
2022-01-25 14:12:02 -0600

    adds orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,37 @@
+class OrdersController < ApplicationController
+
+  def index
+    order1 = current_user.orders
+    render json: order1
+  end
+
+  def create
+    product1 = Product.find(params[:product_id])
+    subtotal = product1.price * params[:quantity]
+    tax = subtotal * 0.1
+    total = subtotal + tax
+    order1 = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total
+    )
+    if order1.save
+      render json: order1
+    else 
+      render json: {errors: order1.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    order1 = current_user.orders.find(params[:id])
+    if order1
+      render json: order1
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
 
   belongs_to :supplier
   has_many :images
+  has_many :orders
 
   def is_discounted?
     #price < 19, this is all this needs
