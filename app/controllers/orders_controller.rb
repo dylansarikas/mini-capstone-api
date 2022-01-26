5260880d5e7a765551ced22f5243a91a7ef08368
dylan <dylan.sarikas@gmail.com>
2022-01-26 11:52:43 -0600

    adds authorization to orders create and index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,35 @@
 class OrdersController < ApplicationController
 
   def index
-    order1 = current_user.orders
-    render json: order1
+    if current_user
+      order1 = current_user.orders
+      render json: order1
+    else
+      render json: {message: "log in correctly"}, status: :unauthorized
+    end
   end
 
   def create
-    product1 = Product.find(params[:product_id])
-    subtotal = product1.price * params[:quantity]
-    tax = subtotal * 0.1
-    total = subtotal + tax
-    order1 = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total
-    )
-    if order1.save
-      render json: order1
-    else 
-      render json: {errors: order1.errors.full_messages}, status: :unprocessable_entity
+    if current_user
+      product1 = Product.find(params[:product_id])
+      subtotal = product1.price * params[:quantity]
+      tax = subtotal * 0.1
+      total = subtotal + tax
+      order1 = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: subtotal,
+        tax: tax,
+        total: total
+      )
+      if order1.save
+        render json: order1
+      else 
+        render json: {errors: order1.errors.full_messages}, status: :unprocessable_entity
+      end
+    else
+      render json: {message: "log in correctly"}, status: :unauthorized
     end
   end
 
